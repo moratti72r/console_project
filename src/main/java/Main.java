884ac87004716019919258c630b9685118db305a
Marat <marrrat1991@yandex.ru>
2023-12-22 07:08:47 +0500

    2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final String PATH_TEXT_FILE = "src\\main\\resources\\";
+//    private static final String PATH_TEXT_FILE = "src\\main\\resources\\";
 
     public static void main(String[] args) throws FileNotFoundException {
 
         Scanner scanner = new Scanner(System.in);
 
-        FileManager fileManager = new FileManager(PATH_TEXT_FILE + scanner.next());
+        FileManager fileManager = new FileManager(/*PATH_TEXT_FILE + */scanner.next());
 
         for (String result : fileManager.firstThreeWords()) {
             System.out.println(result);
